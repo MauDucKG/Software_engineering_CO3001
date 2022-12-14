1a374858a0eab2a94d63d3fdd2520c876061dc07
Nguyễn Mậu Đức <nguyenmauduc91@gmail.com>
2022-12-14 22:26:29 +0700

    duc: remove conflix

--- frontend/src/EditorCustomField/index.js
+++ frontend/src/EditorCustomField/index.js
@@ -2,7 +2,6 @@ import "./index.css";
 import * as React from "react";
 import axios from "axios";
 import Chat from "../Chat";
-import Chat from "../Chat";
 import { useEffect, useState } from "react";
 import { createElement, extend } from "@syncfusion/ej2-base";
 import {
